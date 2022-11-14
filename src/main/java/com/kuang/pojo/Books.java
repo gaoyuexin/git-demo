0c518c3718df224a6f6a66d05110f5e1db4aa011
gaoyuexin <116626184+gaoyuexin@users.noreply.github.com>
2022-11-14 20:03:47 +0800

    111111

--- src/main/java/com/kuang/pojo/Books.java
+++ src/main/java/com/kuang/pojo/Books.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor//data注解除了有参构造都有了，@AllArgsConstructor注解加上有参构造但是无参构造就没了，@NoArgsConstructor加上无参构造
 public class Books {
+    //添加了一行注释
     private int bookID;
     private String bookName;
     private int bookCounts;
